fb3134bfd0723cdd90fe5aca139a1de2478857a0
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-07 11:47:52 -0400

    wrote our Dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,38 @@
+import java.util.Random;
 public class Dice {
+
+    public static void main(String[] args){
+
+        Dice dice = new Dice(10, 20); //test cases
+        System.out.println(dice.tossAndSum());
+    }
+     int numDice;
+     int numSides;
+
+    Dice(int numOfDice, int numOfSides){
+        this.numDice = numOfDice;
+        this.numSides = numOfSides;
+    }
+
+    Dice (int numOfDice) {
+        this.numDice = numOfDice;
+        this.numSides = 6;
+    }
+
+    public  int getRoll(){
+        Random random = new Random();
+        return (random.nextInt(this.numSides)+1);
+    }
+
+    public int tossAndSum(){
+        int sum = 0;
+
+        for (int i = 0; i < this.numDice; i++){
+            sum += getRoll();
+
+        }
+        return sum;
+    }
 
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,8 @@
 public class Simulation {
 
+    public static void main(String[] args) {
 
+        //Dice dice1 = new Dice(2);
+    }
 
 }
